470ee992857dabdf21fade8a2b9517ba1cfd894f
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-04-21 22:09:30 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,5 @@
 # Write your #display_rainbow method here
+
+def #display_rainbow(array)
+  puts "R: #{array[0]}, O: #{array[0]}, Y:#{array[0]} , B:#{array[0]} , I:#{array[0]} , V:#{array[0]} ,}"
+end
